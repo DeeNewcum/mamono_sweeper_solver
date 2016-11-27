3cc59a07c83d823ad30edc1ca56f69a388ecef74
Dee Newcum <dee.newcum@gmail.com>
2016-11-27 09:28:03 -0600

    .

--- Board.py
+++ Board.py
@@ -3,9 +3,27 @@ import pprint
 class Board:
 
     
-    def __init__(self, width, height, is_blind):
+    def __init__(self, width, height, num_mines, is_blind):
         self.width = width
         self.height = height
         self.is_blind = is_blind        # win criteria -- is this is "blind" style winning criteria?
-        self.grid = [[0] * width] * height
+        self.num_covered = width * height
+        self.mine_grid = [[0] * width] * height
+        self.clue_grid = [[0] * width] * height
+        self.is_uncovered = [[False] * width] * height
+        self.marked = [[0] * width] * height    # 0 = unmarked, 1+ = what it's marked as
 
+    def randomly_place_mines(self, num_mines):
+        self.num_covered = width * height
+        self.mine_grid = [[0] * width] * height
+        self.clue_grid = [[0] * width] * height
+        self.is_uncovered = [[False] * width] * height
+        self.marked = [[0] * width] * height
+        # TODO
+
+    def uncover(self, x, y):
+        # TODO
+        
+    def display(self):
+        # display to STDOUT
+        # TODO
